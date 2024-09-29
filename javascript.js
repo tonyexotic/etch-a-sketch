c3bc08d767c413057b73b7a0167af6b26a9205fd
tonyexotic <72349017+tonyexotic@users.noreply.github.com>
2024-09-29 22:03:09 +0200

    Add function to create grid cells (didn't save last commit).

--- javascript.js
+++ javascript.js
@@ -2,3 +2,18 @@ const gridside = 600;
 const columns = 16;
 const rows = 16;
 
+const sketchArea = document.querySelector("#sketch-area");
+
+function createGridCell() {
+    for (i = 0; i < (rows * cells); i++) {
+        const gridCell = document.createElement('div');
+        
+        gridCell.style.width = `${(gridside / columns) - 2} px`;
+        gridCell.style.height = `${(gridside / rows - 2)} px`;
+        gridCell.classList.add("cell");
+
+        sketchArea.appendChild(gridCell);
+    }
+}
+
+createGridCell();
